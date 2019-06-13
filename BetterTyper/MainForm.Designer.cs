95ae2e4a027bf6adf700e3041e88ae134978d814
Kyle Ebbinga <kebbin01@outlook.com>
2019-06-13 00:00:11 -0500

    Add project files.

---
+++ BetterTyper/MainForm.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace BetterTyper
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.coolDownRaw = new System.Windows.Forms.NumericUpDown();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.filePathTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.consoleDisplay = new System.Windows.Forms.ListView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewHelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changelogToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.coolDownRaw)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.coolDownRaw);
+            this.groupBox1.Controls.Add(this.richTextBox2);
+            this.groupBox1.Controls.Add(this.stopButton);
+            this.groupBox1.Controls.Add(this.startButton);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(597, 63);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Typing Controls";
+            // 
+            // coolDownRaw
+            // 
+            this.coolDownRaw.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.coolDownRaw.Location = new System.Drawing.Point(517, 31);
+            this.coolDownRaw.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
+            this.coolDownRaw.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.coolDownRaw.Name = "coolDownRaw";
+            this.coolDownRaw.Size = new System.Drawing.Size(74, 20);
+            this.coolDownRaw.TabIndex = 4;
+            this.coolDownRaw.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.coolDownRaw.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Location = new System.Drawing.Point(389, 34);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(121, 19);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "Cool Down (milliseconds)";
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(99, 29);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 1;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(7, 29);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.saveButton);
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Controls.Add(this.filePathTextBox);
+            this.groupBox2.Location = new System.Drawing.Point(12, 96);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(597, 56);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Set Up";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveButton.Location = new System.Drawing.Point(516, 25);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 22);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(7, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(50, 19);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "File Path";
+            // 
+            // filePathTextBox
+            // 
+            this.filePathTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.filePathTextBox.Location = new System.Drawing.Point(63, 25);
+            this.filePathTextBox.Name = "filePathTextBox";
+            this.filePathTextBox.Size = new System.Drawing.Size(447, 20);
+            this.filePathTextBox.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.consoleDisplay);
+            this.groupBox3.Location = new System.Drawing.Point(12, 158);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(597, 346);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Console";
+            // 
+            // consoleDisplay
+            // 
+            this.consoleDisplay.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.consoleDisplay.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            this.consoleDisplay.Location = new System.Drawing.Point(7, 19);
+            this.consoleDisplay.Name = "consoleDisplay";
+            this.consoleDisplay.Size = new System.Drawing.Size(584, 321);
+            this.consoleDisplay.TabIndex = 0;
+            this.consoleDisplay.UseCompatibleStateImageBehavior = false;
+            this.consoleDisplay.View = System.Windows.Forms.View.Details;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(623, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewHelpToolStripMenuItem,
+            this.changelogToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // viewHelpToolStripMenuItem
+            // 
+            this.viewHelpToolStripMenuItem.Name = "viewHelpToolStripMenuItem";
+            this.viewHelpToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.viewHelpToolStripMenuItem.Text = "View Help";
+            this.viewHelpToolStripMenuItem.Click += new System.EventHandler(this.viewHelpToolStripMenuItem_Click);
+            // 
+            // changelogToolStripMenuItem
+            // 
+            this.changelogToolStripMenuItem.Name = "changelogToolStripMenuItem";
+            this.changelogToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.changelogToolStripMenuItem.Text = "Changelog";
+            this.changelogToolStripMenuItem.Click += new System.EventHandler(this.changelogToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(68, 510);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(541, 23);
+            this.progressBar.TabIndex = 5;
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBox3.BackColor = System.Drawing.SystemColors.Menu;
+            this.richTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox3.Location = new System.Drawing.Point(12, 514);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(50, 19);
+            this.richTextBox3.TabIndex = 6;
+            this.richTextBox3.Text = "Progress";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 545);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(639, 584);
+            this.Name = "mainForm";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.coolDownRaw)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox filePathTextBox;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListView consoleDisplay;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.NumericUpDown coolDownRaw;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewHelpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changelogToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+    }
+}
+
---
+++ BetterTyper/MainForm.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Controls;
+
+namespace BetterTyper
+{
+    public partial class mainForm : Form
+    {
+        public StreamWriter _swLog;
+
+        public int eStop = 0;
+        public int progressPart = 0;
+        public int progressWhole = 0;
+
+        public string versionNumber = "v3.5";
+        public string logFilePath = "C:\\Auto Typer\\latest_log.txt";
+
+        public mainForm()
+        {
+            Text = "Auto Typer " + versionNumber;
+            InitializeComponent();
+            ListView();
+            richTextBox1.ReadOnly = true;
+            richTextBox2.ReadOnly = true;
+            richTextBox3.ReadOnly = true;
+
+            stopButton.Enabled = false;
+
+            bool exists = Directory.Exists(logFilePath);
+
+            try
+            {
+                _swLog = new StreamWriter(logFilePath);
+
+                //if (!File.Exists(logFilePath))
+                //{
+                //    Directory.CreateDirectory(logFilePath);
+                //    Directory.CreateDirectory(logFilePath);
+                //}
+
+                StreamReader sr = new StreamReader("C:\\Auto Typer\\save.txt");
+                string line = sr.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(line.ToString()))
+                {
+                    sr.Close();
+                    return;
+                }
+
+                filePathTextBox.Text = line.ToString();
+
+                sr.Close();
+            }
+            catch
+            {
+                //if (!File.Exists(logFilePath))
+                //{
+                //    Directory.CreateDirectory(logFilePath);
+                //}
+                return;
+            }
+        }
+
+        public void StopProgram()
+        {
+            startButton.Enabled = true;
+            stopButton.Enabled = false;
+            eStop = 1;
+            Thread.Sleep(1000);
+            filePathTextBox.ReadOnly = false;
+            richTextBox3.Text = "Cancelled";
+        }
+
+        public void ListView()
+        {
+            ColumnHeader header = new ColumnHeader();
+            header.Text = "";
+            header.Name = "col1";
+            header.Width = this.Width - 100;
+            consoleDisplay.Columns.Add(header);
+            consoleDisplay.View = View.Details;
+            consoleDisplay.HeaderStyle = ColumnHeaderStyle.None;
+        }
+
+        public void GetProgressWhole()
+        {
+            progressWhole = 0;
+
+            try
+            {
+                string filePath = filePathTextBox.Text.ToString();
+
+                StreamReader sr = new StreamReader(filePath);
+
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    progressWhole++;
+                }
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            int coolDown = (int)coolDownRaw.Value;
+            eStop = 0;
+            progressPart = 0;
+            GetProgressWhole();
+
+            startButton.Enabled = false;
+            stopButton.Enabled = true;
+            saveButton.Enabled = false;
+            filePathTextBox.ReadOnly = true;
+
+            progressBar.Maximum = progressWhole;
+            richTextBox3.Text = "Progress";
+
+            if (string.IsNullOrWhiteSpace(filePathTextBox.Text))
+            {
+                consoleDisplay.Items.Add("Error: No set file location");
+                _swLog.WriteLine("Error: No set file location");
+                _swLog.Flush();
+                return;
+            }
+
+            Thread.Sleep(1500);
+
+            consoleDisplay.Items.Add("Info: Running...");
+            _swLog.WriteLine("Info: Running...");
+            _swLog.Flush();
+
+            var list = new List<string>();
+
+            string filePath = filePathTextBox.Text.ToString();
+
+            using (var sr = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (eStop != 1)
+                    {
+                        list.Add(line);
+                        SendKeys.SendWait("{ENTER}");
+
+                        consoleDisplay.Items.Add("Sending: " + line);
+                        _swLog.WriteLine("Sending: " + line);
+                        _swLog.Flush();
+
+                        SendKeys.SendWait(line + "{ENTER}");
+                        Thread.Sleep(Convert.ToInt32(coolDown));
+
+                        progressPart++;
+                        progressBar.Value = progressPart;
+
+                        consoleDisplay.EnsureVisible(consoleDisplay.Items.Count - 1);
+                    }
+                    else
+                    {
+                        StopProgram();
+                        return;
+                    }
+                }
+
+                StopProgram();
+                richTextBox3.Text = "Done";
+
+            }
+
+        }
+
+        public void Save()
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter("C:\\Auto Typer\\save.txt");
+
+                string filePathString = filePathTextBox.Text.ToString();
+                sw.WriteLine(filePathString);
+
+                sw.Close();
+
+                StreamReader sr = new StreamReader("C:\\Auto Typer\\save.txt");
+                string line = sr.ReadLine();
+
+                if (line == filePathString)
+                {
+                    consoleDisplay.Items.Add("Info: Successfully Saved");
+                    _swLog.WriteLine("Info: Successfully Saved");
+                    _swLog.Flush();
+                    sr.Close();
+                    return;
+                }
+                else
+                {
+                    consoleDisplay.Items.Add("Error: File Not Saved Properly");
+                    _swLog.WriteLine("Error: File Not Saved Properly");
+                    _swLog.Flush();
+                    sr.Close();
+                    return;
+                }
+            }
+            catch
+            {
+                Directory.CreateDirectory("C:\\Auto Typer");
+                consoleDisplay.Items.Add("Error: File not found. Creating directory");
+                consoleDisplay.Items.Add("Info: New directory created. Try saving again");
+                _swLog = new StreamWriter(logFilePath);
+                _swLog.WriteLine("Error: File not found. Creating directory");
+                _swLog.WriteLine("Info: New directory created. Try saving again");
+                _swLog.Flush();
+            }
+        }
+
+        public void ChangeLogText()
+        {
+            MessageBox.Show("Current Changelog for Auto Typer " + versionNumber +
+                "\r\n\r\nAdded Main Strip" +
+                "\r\nAdded ChangeLog button" +
+                "\r\nAdded latest_log.txt" +
+                "\r\nAdded About button" +
+                "\r\nAdded Exit button" +
+                "\r\nAdded Progress bar" +
+                "\r\n\r\nImproved save feature");
+        }
+
+        public void Help()
+        {
+            MessageBox.Show("To save file location for easier use go to your C: drive and create folder called 'Auto Typer'. The program will auto-complete the rest." +
+                "\r\n\r\n'Start' Starts typing.\r\n\r\n'Stop' Stops typing." +
+                "\r\n\r\n'Cool Down' Time between each line typed. For applications with spam protected");
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            StopProgram();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void viewHelpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Help();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Auto Typer by Kyle (TheGuitarleader) " + versionNumber);
+        }
+
+        private void changelogToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChangeLogText();
+        }
+    }
+}
